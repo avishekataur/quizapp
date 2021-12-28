dde8229cf78733f2230b54804fbee883cd1ae3a8
avishekataur <avishekataur@gmail.com>
2021-12-28 08:37:53 +0600

    it-seems-good-idk

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./Pages/Home";
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 };
